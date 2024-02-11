620707ec2d918b42971f03ad22c9e4941ed49f6f
Nick Laws <laws.nick@gmail.com>
2024-02-10 19:54:21 -0700

    Update test_conductor.jl

--- test/test_conductor.jl
+++ test/test_conductor.jl
@@ -1,13 +1,46 @@
 @testset "Conductor single phase" begin
+
+    # some single phase conductors to test with, constructed two different ways
     c1 = CommonOPF.Conductor(; busses=("b1", "b2"), name="edge1", template="edge2", length=1.2)
     c2 = CommonOPF.Conductor(;
         Dict(:busses => ("b2", "b3"), :name => "edge2", :r1 => 0.1, :x1 => 0.2, :length => 20)...
     )
-    CommonOPF.warn_singlephase_conductors_and_copy_templates([c1, c2])
-    @test resistance_per_length(c1) == resistance_per_length(c2) == 0.1
-    @test resistance(c1) == 0.1 * 1.2
-    @test resistance(c2) == 0.1 * 20
-    @test reactance_per_length(c1) == reactance_per_length(c2) == 0.2
-    @test reactance(c1) == 0.2 * 1.2
-    @test reactance(c2) == 0.2 * 20
+
+    @testset "warn_singlephase_conductors_and_copy_templates" begin
+        CommonOPF.warn_singlephase_conductors_and_copy_templates([c1, c2])
+        @test resistance_per_length(c1) == resistance_per_length(c2) == 0.1
+        @test resistance(c1) == 0.1 * 1.2
+        @test resistance(c2) == 0.1 * 20
+        @test reactance_per_length(c1) == reactance_per_length(c2) == 0.2
+        @test reactance(c1) == 0.2 * 1.2
+        @test reactance(c2) == 0.2 * 20
+    end
+
+    @testset "check_edges! for single phase Conductors" begin
+        # check_edges! should do excactly what warn_singlephase_conductors_and_copy_templates did 
+        CommonOPF.check_edges!([c1, c2])
+        @test resistance_per_length(c1) == resistance_per_length(c2) == 0.1
+        @test resistance(c1) == 0.1 * 1.2
+        @test resistance(c2) == 0.1 * 20
+        @test reactance_per_length(c1) == reactance_per_length(c2) == 0.2
+        @test reactance(c1) == 0.2 * 1.2
+        @test reactance(c2) == 0.2 * 20
+    end
+
+    # some multi-phase conductors to test with
+    c1 = CommonOPF.Conductor(; 
+        busses=("b1", "b2"), name="edge1", template="edge2", length=1.2, phases=[2,3]
+    )
+    c2 = CommonOPF.Conductor(;
+        Dict(
+            :busses => ("b2", "b3"), :name => "edge2", :r1 => 0.1, :x1 => 0.2, :length => 20,
+            :phases => [1]
+        )...
+    )
+    clear_log!(test_logger)
+    CommonOPF.validate_multiphase_conductors!([c1, c2])
+    @test occursin(
+        "do not have sufficient parameters to define the impedance", 
+        test_logger.logs[end].message
+    )
 end
