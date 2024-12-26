c9fa44673cb5b2e26f8cc11abf8e5a484d837a04
Nick Laws <laws.nick@gmail.com>
2024-12-26 14:20:16 -0600

    Update test_model_construction_utils.jl

--- test/test_model_construction_utils.jl
+++ test/test_model_construction_utils.jl
@@ -14,4 +14,18 @@
     phases = [1, 2, 3]
     v = matrix_phases_to_vec(M, phases)
     @test v == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    # phi_ij
+    fp = joinpath("data", "yaml_inputs", "ieee13_multi_phase.yaml")
+    net = Network(fp)
+    # bus 684 has only phases [1, 3]
+    m = phi_ij("684", net, M)
+    @test m[1,1] == M[1,1]
+    @test m[2,2] == 0  # diagonal values real
+    @test m[1,2] == 0im  # off-diagonal values complex
+
+    v = [1im, 2, 3]
+    v = phi_ij("684", net, v)
+    @test v[1] == 1im
+    @test v[2] == 0im
 end
